caf7e3e568469ceacc90d75e4ceb7b9ef91892af
Nindenkawe <amiritius@gmail.com>
2021-11-25 15:46:25 +0200

    new

---
+++ ReadSerialData.py
@@ -0,0 +1,26 @@
+#Require modules,
+import json, serial, time
+
+
+ 
+#json doc file
+doc ={
+    "doc_packet": [],
+}
+
+doc = json.dumps(doc)
+
+file = open ('on_street_doc.json', 'a')
+file.write(doc)
+file.close()
+
+doc = serial.Serial('COM3' ,115200)
+time.sleep(1)
+
+
+while True:
+    while (doc.inWaiting() == 0):
+        pass
+    doc_packet = doc.readline()
+    doc_packet = str(doc_packet, 'utf8')
+    print(doc_packet)
